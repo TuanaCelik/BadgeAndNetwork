efdbe5c304ee425940fef24575e295b641a00c1d
ISABEL <iren@seas.upenn.edu>
2017-04-23 13:56:21 +0800

    outputting new data structure with dest, added comm_size, count

--- public/python_scripts.py
+++ public/python_scripts.py
@@ -1,4 +1,5 @@
 import csv
+from collections import Counter
 
 def put_csv_into_list(file_name):
   data = []
@@ -11,6 +12,45 @@ def put_csv_into_list(file_name):
 
 IPLogData = put_csv_into_list('IPLog.csv')
 
+#input should be IPLogData (source, dest, reqsize, ressize, commsize)
+def add_comm_sizes(logdata):
+  comm_size_dict = {}
+  counter_dict = {}
+  for i in logdata:
+    dest = i[1]
+    if dest not in comm_size_dict:
+      comm_size_dict[dest] = float(i[4])
+      counter_dict[dest] = 1
+    else:
+      comm_size_dict[dest] = comm_size_dict[dest] + float(i[4])
+      counter_dict[dest] = counter_dict[dest] + 1
+
+  return (comm_size_dict, counter_dict)
+
+(added_comm_sizes, counts) = add_comm_sizes(IPLogData)
+
+def output_data_structure(comm_map, counts):
+  #destination, commSize, count
+  output_list = []
+  for key in comm_map:
+    output_list.append([key, int(comm_map[key]), counts[key]])
+
+  return output_list
+
+final_list = output_data_structure(added_comm_sizes, counts)
+sorted_list = sorted(final_list, key = lambda x: int(x[1]))
+
+def sort_and_cut_list(num, sorted_list):
+  truncated_list = []
+  for i in xrange(1, num + 1):
+    truncated_list.append(sorted_list[-i])
+  return truncated_list
+
+sorted_list = sort_and_cut_list(10, sorted_list)
+
+
+
+
 #how many distinct destination IP addresses there are in IPLog.csv
 #20243 distinct destinations
 #115414 total destinations
@@ -22,27 +62,15 @@ def distinct_destinations():
     spamreader = csv.reader(csvfile)
     first_row = next(spamreader)
     for row in spamreader:
-      count +=1
-      if row[1] not in dest_set:
-        dest_set.add(row[1])
+        count +=1
+        if row[1] not in dest_set:
+          dest_set.add(row[1])
 
-  return len(dest_set)
-  #return count
+  #return len(dest_set)
+  return count
+#print distinct_destinations()
+#new data structure: destination, size, count
+#dictionary: destination: size, count
 
+#top_ten_comm_sizes()
 
-def top_ten_comm_sizes():
-
-  #for i in xrange(10):
-    #print IPLogData[i]
-  f = open('commSize.txt', 'w')
-
-  IPLogData.sort(key=lambda x: x[4])
-  new_list = sorted(IPLogData, key = lambda x: int(x[4]))
-  for i in xrange(1, 11):
-    print i
-    f.write(str(new_list[-i]))
-    f.write('\n')
-  f.close()
-
-
-top_ten_comm_sizes()
